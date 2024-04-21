6a4e5936cb528cfdf0230cd0cdcf74fbe8cf0365
mitsuki.ogura <yu_uy1224@outlook.jp>
2024-04-22 07:09:45 +0900

    DestroyServlet.javaを作成

--- src/controllers/DestroyServlet.java
+++ src/controllers/DestroyServlet.java
@@ -1,7 +1,6 @@
 package controllers;
 
 import java.io.IOException;
-import java.sql.Timestamp;
 
 import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
@@ -14,16 +13,16 @@ import models.Task;
 import utils.DBUtil;
 
 /**
- * Servlet implementation class UpdateServlet
+ * Servlet implementation class DestroyServlet
  */
-@WebServlet("/update")
-public class UpdateServlet extends HttpServlet {
+@WebServlet("/destroy")
+public class DestroyServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UpdateServlet() {
+    public DestroyServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -40,18 +39,8 @@ public class UpdateServlet extends HttpServlet {
             //該当のIDのタスク1件のみをDBから取得
             Task t = em.find(Task.class,(Integer)(request.getSession().getAttribute("task_id")));
 
-
-            //フォームの内容を各フィールドに上書き
-            String title = request.getParameter("title");
-            t.setTitle(title);
-
-            String content = request.getParameter("content");
-            t.setContent(content);
-
-            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-            t.setUpdated_at(currentTime);;
-
             em.getTransaction().begin();
+            em.remove(t);
             em.getTransaction().commit();
             em.close();
 
